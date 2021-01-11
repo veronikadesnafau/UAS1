b4989b32ab37a9e57b7d91f7005a23c697a27c92
faradeviana <faraadevianaa@gmail.com>
2021-01-12 01:27:50 +0700

    uas

---
+++ main.py
@@ -0,0 +1,40 @@
+from view.view_nilai import cari,cetak,header,notoption
+from view.input_nilai import inputan,ubah,hapus
+header()
+while True:
+
+    c = input("\nPilih Opsi: ")
+
+    # EXIT PROGRAM
+    if c.lower() == 'k':
+        print(".:TERIMAKASIH TELAH MENGGUNAKAN PROGRAM INI:.\n"
+              "               Veronika Desna              \n"
+              "                 TI.20.a.A.2.                 ")
+        ext = input("\nPress ENTER to exit")
+        if ext == '':
+            break
+        else:
+            break
+
+    # PRINT DATA
+    elif c.lower() == 'l':
+        cetak()
+
+    # MENAMBAH DATA
+    elif c.lower() == 't':
+        inputan()
+
+    # UBAH DATA
+    elif c.lower() == 'u':
+        ubah()
+
+    # MENCARI DATA
+    elif c.lower() == 'c':
+        cari()
+
+    # HAPUS DATA
+    elif c.lower() == 'h':
+        hapus()
+
+    else:
+        notoption()
